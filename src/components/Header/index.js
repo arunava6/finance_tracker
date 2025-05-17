f662a5441a53d895379a6450604911f9bc54daac
Simplilearn_Github <siddam.bharat@simplilearn.net>
2025-05-18 00:00:22 +0530

    commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./styles.css"
+import { auth } from '../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 function Header() {
-   function logoutFnc() {
-    alert("Logout!");
-   }
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, loading])
+
+  function logoutFnc() {
+    try {
+      signOut(auth).then(() => {
+        toast.success("Logged out successfully");
+        navigate("/");
+      }).catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      })
+    } catch (e) {
+      toast.error(e.message);
+    }
+  }
   return (
     <div className='navbar'>
       <p className='logo'>Financely</p>
-      <p className='logo link' onClick={logoutFnc}>Logout</p>
-    </div>
+      {user && (
+        < p className='logo link' onClick={logoutFnc}>Logout</p>
+      )}
+
+    </div >
   )
 }
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function Dashboard() {
   return (
-    <div>Dashboard Page</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
